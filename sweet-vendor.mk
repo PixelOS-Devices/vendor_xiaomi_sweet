7f978afe007eb53af95b405a5be8417bb9be4239
Ali Hasan <ahb7671@gmail.com>
2022-02-25 05:15:38 -0500

    sweet: vendor overlay manifest to enable radio 1.5
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sweet/proprietary/system_ext/lib64/vendor.qti.ims.rcsconfig@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.ims.rcsconfig@1.0.so \
     vendor/xiaomi/sweet/proprietary/system_ext/lib64/vendor.qti.imsrtpservice@3.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.imsrtpservice@3.0.so \
     vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/bin/hw/android.hardware.power-service:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/bin/hw/android.hardware.power-service \
+    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/etc/vintf/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/etc/vintf/manifest.xml \
     vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/soundfx/libvolumelistener.so \
     vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib64/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib64/soundfx/libvolumelistener.so
 
